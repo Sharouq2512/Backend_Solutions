f74dbf022c98df10bd1a3a93f4ecce2d5b3797f4
Sharoouq Al-Badi <sharouqalbadi12@gmail.com>
2026-04-21 15:22:34 +0400

    Add files via upload

---
+++ solution08/project01/Program.cs
@@ -0,0 +1,316 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*============Arrays===========*/
+
+
+            //int[] numbers = new int [5];
+            //Console.WriteLine(numbers[1]);
+
+
+            //--------------------------------------------------
+
+
+            //string[] friends = { "Shahad", "Tasnim", "Sharouq", "Shima", "Rayan" };
+            //Console.WriteLine(friends[1]);
+
+
+            //--------------------------------------------------
+
+
+            //string[] friends = { "Shahad", "Tasnim", "Sharouq", "Shima", "Rayan" };
+            //for (int i = 0; i < friends.Length; i++)
+            //{
+            //    Console.WriteLine(friends[i]);
+            //}
+
+
+            //-------------------------------------------------- 
+
+
+            //string[] friends = { "Shahad", "Tasnim", "Sharouq", "Shima", "Rayan" };
+
+            //foreach (string element in friends) 
+            //{
+            //    Console.WriteLine(element);
+            //}
+
+
+            //-------------------------------------------------- 
+
+
+            //string[] friends = { "Shahad", "Tasnim", "Sharouq", "Shima", "Rayan" };
+            //Console.WriteLine(friends.Length);
+
+
+            //-------------------------------------------------- 
+
+            //int[] numbers = new int[5];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine("Enter any number:");
+
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //-------------------------------------------------- 
+
+
+
+            //int[] numbers = new int[5];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine("Enter any number:");
+
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine(Array.IndexOf(numbers,45));
+
+
+
+            //-------------------------------------------------- 
+
+
+            //int[] num = {1,2,3,4,5,6,7,8,9};
+            //Console.WriteLine(Array.IndexOf(num, 5));
+
+
+            //-------------------------------------------------- 
+
+
+            //int[] num = { 5, 2, 9, 4, 7, 10, 3, 6, 8 };
+            //Array.Sort(num);
+
+            //foreach (int i in num)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            /*======================Tasks=======================*/
+
+            //Task 1.
+
+            //int[] num = { 10, 20, 30, 40, 50 };
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //-------------------------------------------------- 
+
+            //Task2.
+
+            //int[] num = { 15, 25, 35, 45, 55 };
+            //foreach (int cup in num)
+            //{
+            //    Console.WriteLine(cup);
+            //}
+
+
+            //-------------------------------------------------- 
+
+            //Task3.
+
+            //int[] numbers = new int[5];
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine("Enter any number:");
+
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //-------------------------------------------------- 
+
+            //Task4.
+
+            //int[] numbers = new int[5];
+            //int sum = 0;
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("plz Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            //    sum += numbers[i]; 
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine("Sum = " + sum);
+
+
+
+            //-------------------------------------------------- 
+
+            //Task5.
+
+            //int[] numbers = new int[5];
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("plz Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine($"Maximum number is= " +numbers.Max());
+
+
+            //--------------------------------------------------
+
+            //Task6.
+
+            //int[] numbers = new int[5];
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("plz Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine($"Minimum number is= " + numbers.Min());
+
+
+            //--------------------------------------------------
+
+            //Task7.
+
+            //int[] num = new int[10];
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("Enter number: ");
+            //    num[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine("Even numbers are:");
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (num[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(num[i]);
+            //    }
+            //}
+
+
+            //--------------------------------------------------
+
+            //Task8.
+
+            //int[] numbers = new int[5];
+            //int sum = 0;
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("plz Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+            //    sum += numbers[i];
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine("Sum = " + sum);
+            //Console.WriteLine("Average = " + (sum / 5.0));
+
+
+            //--------------------------------------------------
+
+            //Task9.
+
+            //int[] numbers = new int[5];
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("/*------------------*/");
+
+            //Console.WriteLine("Reverse Number:");
+
+            //for (int i = 4; i >=0 ; i--)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+
+            //--------------------------------------------------
+
+            //Task10.
+
+            //int[] numbers = new int[5];
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write("Enter number: ");
+            //    numbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+
+            //Console.Write("Enter number to search: ");
+            //int target = Convert.ToInt32(Console.ReadLine());
+
+            //int index = -1; 
+
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (numbers[i] == target)
+            //    {
+            //        index = i;
+            //        break;
+            //    }
+            //}
+
+            //if (index != -1)
+            //    Console.WriteLine("Number found at index " + index);
+            //else
+            //    Console.WriteLine("Number not found");
+        }
+    }
+}
---
+++ solution09/project01/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCount = 5;
+            int subjects = 3;
+
+            string[] subjectNames = { "Math", "Science", "English" };
+
+            // =========================
+            // PART 1
+            
+            string[] students = new string[studentsCount];
+
+            Console.WriteLine("Enter 5 student names:");
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.Write($"Student {i + 1}: ");
+                students[i] = Console.ReadLine();
+            }
+
+            // =========================
+            // PART 2
+            
+            int[,] grades = new int[studentsCount, subjects];
+
+            Console.WriteLine("\nEnter grades:");
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                Console.WriteLine($"\n{students[i]}:");
+
+                for (int j = 0; j < subjects; j++)
+                {
+                    Console.Write($"{subjectNames[j]}: ");
+                    grades[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // =========================
+            // PART 3 and 4
+            
+            Console.WriteLine("\n--- Grades Table ---");
+
+            Console.Write("Student\t");
+            for (int j = 0; j < subjects; j++)
+            {
+                Console.Write(subjectNames[j] + "\t");
+            }
+            Console.WriteLine("Average");
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                Console.Write($"{students[i]}\t");
+
+                int sum = 0;
+
+                for (int j = 0; j < subjects; j++)
+                {
+                    Console.Write(grades[i, j] + "\t");
+                    sum += grades[i, j];
+                }
+
+                double avg = (double)sum / subjects;
+                Console.WriteLine(avg);
+            }
+
+            // =========================
+            // PART 5
+            Console.WriteLine("\n--- Jagged Array (from 2D grades) ---");
+
+            int[][] jagged = new int[studentsCount][];
+
+            // convert 2D array → jagged array
+            for (int i = 0; i < studentsCount; i++)
+            {
+                jagged[i] = new int[subjects];
+
+                for (int j = 0; j < subjects; j++)
+                {
+                    jagged[i][j] = grades[i, j];
+                }
+            }
+
+            // display
+            for (int i = 0; i < studentsCount; i++)
+            {
+                Console.WriteLine($"\nStudent: {students[i]}");
+                Console.WriteLine("Number of subjects: " + jagged[i].Length);
+
+                Console.Write("Grades: ");
+
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.Write(jagged[i][j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            //==============================
+            // PART 6
+            Console.Write("\nEnter student name to search: ");
+            string searchName = Console.ReadLine();
+
+            int index = -1;
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                if (students[i].Equals(searchName, StringComparison.OrdinalIgnoreCase))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+                Console.WriteLine($"Student found at index: {index}");
+            else
+                Console.WriteLine("Student not found.");
+
+
+            //======================
+            // PART 7
+            int highest = grades[0, 0];
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                for (int j = 0; j < subjects; j++)
+                {
+                    if (grades[i, j] > highest)
+                    {
+                        highest = grades[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"\nHighest grade in system: {highest}");
+        }
+    }
+}
